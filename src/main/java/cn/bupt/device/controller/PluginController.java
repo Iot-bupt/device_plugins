b61dfe1110d649cd42aa0f5066d1645709010f0c
youzi <liyou@bupt.edu.cn>
2018-05-04 14:17:08 +0800

    liyou:新增发送邮件模块controller

--- src/main/java/cn/bupt/device/controller/PluginController.java
+++ src/main/java/cn/bupt/device/controller/PluginController.java
@@ -1,10 +1,19 @@
 package cn.bupt.device.controller;
 
 
+import cn.bupt.device.sendEmailMethod.SendMail;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/plugin")
 public class PluginController {
+    @RequestMapping(value = "/sendMail", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    public void sendMail(@RequestParam("to[]") String[] to, @RequestParam("subject") String subject, @RequestParam("text") String text) throws Exception {
+        SendMail sendMail=new SendMail();
+        String s=sendMail.sendEmail(to,subject,text);
+    }
 }
+
