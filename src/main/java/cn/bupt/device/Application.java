98de3e1025d566f5f9e4818db2865a9ef0456a39
youzi <liyou@bupt.edu.cn>
2018-05-04 11:09:40 +0800

    liyou:新增发送邮件模块

---
+++ src/main/java/cn/bupt/device/Application.java
@@ -0,0 +1,17 @@
+package cn.bupt.device;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by tangjialiang on 2017/12/19.
+ *
+ */
+
+@SpringBootApplication
+//@MapperScan("com.tjlcsat.mapper")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/cn/bupt/device/controller/PluginController.java
@@ -0,0 +1,10 @@
+package cn.bupt.device.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/plugin")
+public class PluginController {
+}
