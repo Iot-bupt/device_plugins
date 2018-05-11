2f04139593af21a4fdff1b00c58f6b321bbc25a8
tangjialiang <phx108@sina.com>
2018-05-11 12:08:42 +0800

    - update something about swagger2

--- src/main/java/cn/bupt/device/controller/PluginController.java
+++ src/main/java/cn/bupt/device/controller/PluginController.java
@@ -7,6 +7,8 @@ import cn.bupt.device.sendEmailMethod.SendMail;
 import cn.bupt.device.sendEmailMethod.Timer;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +25,13 @@ public class PluginController {
     SendMail sendMail;
 
     @Timer
-    @RequestMapping(value = "/sendMail", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    @ApiOperation(value = "send a mail", notes = "send a mail api")
+    @ApiImplicitParam(name = "jsonStr", value = "{\n" +
+            "\t\"to\": [\"liyou@bupt.edu.cn\"],\n" +
+            "\t\"subject\": \"传感器运行情况报告\",\n" +
+            "\t\"text\": \"运行情况良好，祝你一路顺风\"\n" +
+            "}", required = true)
+    @RequestMapping(value = "/sendMail", method = RequestMethod.POST)
     @ResponseBody
     public String sendMail(@RequestBody String jsonStr) throws Exception {
         JsonObject jsonObj = (JsonObject)new JsonParser().parse(jsonStr);
@@ -37,14 +45,14 @@ public class PluginController {
         return "发送成功";
     }
 
-    @RequestMapping(value = "/active", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    @RequestMapping(value = "/active", method = RequestMethod.POST)
     @ResponseBody
     public String setActive(){
         sendMail.setState("ACTIVE");
         return "Plugin active";
     }
 
-    @RequestMapping(value = "/suspend", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    @RequestMapping(value = "/suspend", method = RequestMethod.POST)
     @ResponseBody
     public String setSuspend(){
         sendMail.setState("SUSPEND");
